5f698c4fe87cd9f8a4bf1c6a1b991d695bd8bd38
Joshua Talib <j.f.talib@pgr.reading.ac.uk>
2017-09-12 16:38:25 +0100

    added FTBS and CTBS

--- burger_equation/main.py
+++ burger_equation/main.py
@@ -8,43 +8,57 @@ execfile("plotting.py")
 if __name__ == "__main__":
 	# function that creates constants needed to initialise model.
 	# put in number of x points, change in time and total number of timesteps.	
-	nx,dt,dx,total_nt,x = initial_constants(200,0.001,100)
+	nx,dt,dx,total_nt,x = initial_constants(200,0.0005,100)
 	initial_phi_curve = initial_curve(x)
 	initial_phi_square = initial_square(x)
 	
 	# FTCS for all time-steps.
 	phi_FTCS, phi = FTCS(initial_phi_square,total_nt)
 
+	# FTBS
+	phi_FTBS, phi = FTBS(initial_phi_square,total_nt)
+
 	# CTCS
 	phi_new, phi = FTCS(initial_phi_square,1)
 	phi_CTCS = CTCS(phi_new,phi,total_nt-1)
 
+	# CTBS
+	phi_new, phi = FTCS(initial_phi_square,1)
+	phi_CTBS = CTBS(phi_new,phi,total_nt-1)
+
 	fig = plt.figure(figsize=[12,12])
 	f, ax = plt.subplots(2, 2, sharex=True,figsize=[12,12])
 
-	plotting_2_by_2_subplot(0,0,x,[initial_phi_square,phi_FTCS,phi_CTCS],\
-				['--','--','--'],['Initial','FTCS','CTCS'],\
-				['r','b','g'],'Initial Square',ylim_limits=[-0.1,1.1],legend='yes')
-	plotting_2_by_2_subplot(0,1,x,[phi_FTCS-phi_CTCS],\
-				['-'],['FTCS - CTCS'],\
-				['k'],'Difference')
+	plotting_2_by_2_subplot(0,0,x,[initial_phi_square,phi_FTCS,phi_FTBS,phi_CTCS,phi_CTBS],\
+				['-','--','--','--','--'],['Initial','FTCS','FTBS','CTCS','CTBS'],\
+				['r','b','purple','g','orange'],'Initial Square',ylim_limits=[-0.1,1.1],legend='yes')
+	plotting_2_by_2_subplot(0,1,x,[phi_FTCS-phi_CTCS,phi_FTCS-phi_FTBS,phi_FTCS-phi_CTBS],\
+				['-','-','-'],['FTCS - CTCS','FTCS - FTBS','FTCS - CTBS'],\
+				['k','r','orange'],'Difference',legend='yes')
 	    
 	# FTCS for all time-steps.
 	phi_FTCS, phi = FTCS(initial_phi_curve,total_nt)
+
+	# FTBS
+	phi_FTBS, phi = FTBS(initial_phi_curve,total_nt)
 
 	# CTCS
 	phi_new, phi = FTCS(initial_phi_curve,1)
 	phi_CTCS = CTCS(phi_new,phi,total_nt-1)
 
-	plotting_2_by_2_subplot(1,0,x,[initial_phi_curve,phi_FTCS,phi_CTCS],\
-				['--','--','--'],['Initial','FTCS','CTCS'],\
-				['r','b','g'],'Initial Curve',ylim_limits=[-0.1,1.1],legend='yes')
+	# CTBS
+	phi_new, phi = FTCS(initial_phi_curve,1)
+	phi_CTBS = CTBS(phi_new,phi,total_nt-1)
 
-	plotting_2_by_2_subplot(1,1,x,[phi_FTCS-phi_CTCS],\
-				['-'],['FTCS - CTCS'],\
-				['k'],'Difference')
+	plotting_2_by_2_subplot(1,0,x,[initial_phi_curve,phi_FTCS,phi_FTBS,phi_CTCS,phi_CTBS],\
+				['-','--','--','--','--'],['Initial','FTCS','FTBS','CTCS','CTBS'],\
+				['r','b','purple','g','orange'],'Initial Curve',ylim_limits=[-0.1,1.1],legend='yes')
 
-	plt.suptitle('Burger Number Investigation - JT',fontsize=12.0)
+	plotting_2_by_2_subplot(1,1,x,[phi_FTCS-phi_CTCS,phi_FTCS-phi_FTBS,phi_FTCS-phi_CTBS],\
+				['-','-','-'],['FTCS - CTCS','FTCS - FTBS','FTCS - CTBS'],\
+				['k','r','orange'],'Difference',legend='yes')
+
+	plt.suptitle('Burger Number - JT, nx = '+str(nx)+', dt = '+str(dt)+', nt = '+str(total_nt),fontsize=12.0)
 	plt.tight_layout()
 	plt.show()
 	plt.close()
--- burger_equation/numerical_methods.py
+++ burger_equation/numerical_methods.py
@@ -31,3 +31,30 @@ def CTCS(phi,phi_old,nt):
         phi = phi_New.copy()
     return phi_New
 
+def FTBS(phi,nt):
+    phi_New = phi.copy()
+    for t in xrange(0,nt):
+        max_c = 0.0
+        for j in xrange(1,nx):
+            #print (j)
+            c = (phi[j]*dt)/dx
+            max_c = np.max([c,max_c]) 
+            phi_New[j] = phi[j] - c*(phi[j]-phi[j-1])
+        print ('maximum c number at ts ', t, ' is ', max_c)
+        # boundary conditions
+        phi_New[0] = phi[0] - c*(phi[0]-phi[nx-1])
+        phi[nx] = phi[0]
+        phi = phi_New.copy()
+    return phi_New,phi
+    
+def CTBS(phi,phi_old,nt):
+    phi_New = phi.copy()
+    for t in xrange(0,nt):
+        for j in xrange(1,nx):
+            c = (phi[j]*dt)/dx
+            phi_New[j] = phi_old[j] - 2.*c*(phi[j]-phi[j-1])
+        phi_New[0] = phi_old[0] - 2.*c*(phi[0]-phi[nx-1])
+        phi_New[nx] = phi_New[0]
+        phi_old = phi.copy()
+        phi = phi_New.copy()
+    return phi_New
